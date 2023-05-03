42f6757e583ced17badd0ca698fba033636277ee
Adeshina Adenuga <Nuga0909@gmail.com>
2023-05-03 14:19:54 +0100

    class and functional states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,72 @@
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Comp/Person';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
 
-export default App;
+
+// class App extends Component {
+//   state = {
+//     persons: [
+//       { name: "Maxy", age: "23" },
+//       { name: "Maxaine", age: "25" },
+//       { name: "Ayo", age: "29" }
+//     ]
+//   }
+
+//   switchNameHandler = () => {
+//     this.setState({
+//       persons: [
+//         { name: "Maxo", age: "23" },
+//         { name: "Maxainoo", age: "25" },
+//         { name: "Ayoxy", age: "29" }
+//       ]
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hi, I am a react App</h1>
+//         <button onClick={this.switchNameHandler}>Switch name</button>
+//         <Person name={personState.persons[0].name} age={personState.persons[0].age} />
+//         <Person name={personState.persons[1].name} age={personState.persons[1].age} />
+//         <Person name={personState.persons[2].name} age={personState.persons[2].age} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+// const App = props => {
+//   const [personState, setPersonState] = useState({
+//     persons: [
+//       { name: "Maxy", age: "23" },
+//       { name: "Maxaine", age: "25" },
+//       { name: "Ayo", age: "29" }
+//     ]
+//   });
+
+//   const switchNameHandler = () => {
+//     setPersonState({
+//       persons: [
+//         { name: "Maxo", age: "23" },
+//         { name: "Maxainoo", age: "25" },
+//         { name: "Ayoxy", age: "29" }
+//       ]
+//     })
+//   }
+
+//   return (
+//     <div className="App">
+//       <h1>Hi, I am a react App</h1>
+//       <button onClick={switchNameHandler}>Switch name</button>
+//       <Person name={personState.persons[0].name} age={personState.persons[0].age} />
+//       <Person name={personState.persons[1].name} age={personState.persons[1].age} />
+//       <Person name={personState.persons[2].name} age={personState.persons[2].age} />
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/Comp/Person.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Person = (props) => {
+    return (
+    <>
+        <p>I am {props.name} and I am {props.age} years old</p>
+    </>
+    )
+}
+
+export default Person;
