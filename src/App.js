4175f6b7db7c400a093b31666a473e95ab8a209c
Adeshina Adenuga <Nuga0909@gmail.com>
2023-05-04 18:06:17 +0100

    use map to list array

--- src/App.js
+++ src/App.js
@@ -49,11 +49,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler} />
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.onChangedHandler} />
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+          {this.state.persons.map(person => {
+            return <person name={person.name} age={person.age}/>
+          })}
         </div>
-      )
+      );
     }
 
     return (
@@ -67,36 +67,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-// const App = props => {
-//   const [personState, setPersonState] = useState({
-//     persons: [
-//       { name: "Maxy", age: "23" },
-//       { name: "Maxaine", age: "25" },
-//       { name: "Ayo", age: "29" }
-//     ]
-//   });
-
-//   const switchNameHandler = () => {
-//     setPersonState({
-//       persons: [
-//         { name: "Maxo", age: "23" },
-//         { name: "Maxainoo", age: "25" },
-//         { name: "Ayoxy", age: "29" }
-//       ]
-//     })
-//   }
-
-//   return (
-//     <div className="App">
-//       <h1>Hi, I am a react App</h1>
-//       <button onClick={switchNameHandler}>Switch name</button>
-//       <Person name={personState.persons[0].name} age={personState.persons[0].age} />
-//       <Person name={personState.persons[1].name} age={personState.persons[1].age} />
-//       <Person name={personState.persons[2].name} age={personState.persons[2].age} />
-//     </div>
-//   );
-// }
-
-// export default App;
